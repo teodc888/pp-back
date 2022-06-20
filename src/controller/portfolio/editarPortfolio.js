6e7673d6915e886a27848f49c9ead01101645ed3
mateo <teodc4447@gmail.com>
2022-06-20 20:23:24 -0300

    agregando editar proyectos portfolio

---
+++ src/controller/portfolio/editarPortfolio.js
@@ -0,0 +1,26 @@
+const { Portfolios } = require("../../db.js");
+
+async function editarPortfolio(req, res, next) {
+  try {
+    const { titulo, descripcion, imagen, link, github, id } = req.body;
+    const portfolio = await Portfolios.findOne({ where: { id } });
+
+    if (!portfolio)
+      return res.status(404).send({ message: "Product not found" });
+
+    await portfolio.update({
+      titulo,
+      descripcion,
+      imagen,
+      link,
+      github,
+    });
+
+    return res.status(200).send({ message: "Product updated" });
+    
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { editarPortfolio };
--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const {createPortfolio} = require("../controller/portfolio/createPortfolio");
-const {mostrarPortfolio} = require("../controller/portfolio/getPortafolio");
-const {deletePortfolio} = require("../controller/portfolio/deletePortfolio");
+const { createPortfolio } = require("../controller/portfolio/createPortfolio");
+const { mostrarPortfolio } = require("../controller/portfolio/getPortafolio");
+const { deletePortfolio } = require("../controller/portfolio/deletePortfolio");
+const { editarPortfolio } = require("../controller/portfolio/editarPortfolio");
 
 router.post("/", createPortfolio);
 router.get("/", mostrarPortfolio);
 router.delete("/", deletePortfolio);
+router.put("/", editarPortfolio);
 
 module.exports = router;
